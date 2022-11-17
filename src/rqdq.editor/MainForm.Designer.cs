d057e0ba3206180c9bfbf78b65e2356707f3cb74
Benjamin Yates <benjamin@rqdq.com>
2022-11-17 12:35:55 -0600

    try RenderControl in a form (i.e. editor)

---
+++ src/rqdq.editor/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace rqdq.editor {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.renderControl1 = new SharpDX.Windows.RenderControl();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 40);
+      this.splitContainer1.Margin = new System.Windows.Forms.Padding(6);
+      this.splitContainer1.Name = "splitContainer1";
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.renderControl1);
+      this.splitContainer1.Size = new System.Drawing.Size(1486, 660);
+      this.splitContainer1.SplitterDistance = 494;
+      this.splitContainer1.SplitterWidth = 7;
+      this.splitContainer1.TabIndex = 0;
+      // 
+      // renderControl1
+      // 
+      this.renderControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.renderControl1.Location = new System.Drawing.Point(0, 0);
+      this.renderControl1.Margin = new System.Windows.Forms.Padding(6);
+      this.renderControl1.Name = "renderControl1";
+      this.renderControl1.Size = new System.Drawing.Size(985, 660);
+      this.renderControl1.TabIndex = 0;
+      this.renderControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.renderControl1_Paint);
+      this.renderControl1.Resize += new System.EventHandler(this.renderControl1_Resize);
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Interval = 16;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(1486, 40);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(71, 36);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // quitToolStripMenuItem
+      // 
+      this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+      this.quitToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+      this.quitToolStripMenuItem.Text = "Quit";
+      this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1486, 700);
+      this.Controls.Add(this.splitContainer1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(6);
+      this.Name = "MainForm";
+      this.Text = "MainForm";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private SharpDX.Windows.RenderControl renderControl1;
+        private System.Windows.Forms.Timer timer1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
---
+++ src/rqdq.editor/MainForm.cs
@@ -0,0 +1,157 @@
+﻿using System.Diagnostics;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System.Numerics;
+using SharpDX.Mathematics.Interop;
+
+namespace rqdq.editor {
+
+public partial
+class MainForm : Form {
+  private SharpDX.Direct3D11.Device? _gpu;
+  private SwapChainDescription _desc;
+  private SwapChain? _sc;
+  private Texture2D? _renderTarget;
+  private Texture2D? _depthBuffer;
+  private RenderTargetView? _renderTargetView;
+  private DepthStencilView? _depthBufferView;
+  private bool _renderResized = false;
+
+  private readonly string _dataDir;
+  private readonly string _sceneFileName;
+  private readonly scene.SystemValues _systemValues = new("system");
+  private readonly List<scene.Node> _builtins = new();
+  private readonly Stopwatch _clock = Stopwatch.StartNew();
+
+  private DateTime _mtime;
+  private scene.SceneGraph? _runningGraph;
+
+  public
+  MainForm() {
+    _dataDir = System.Environment.GetEnvironmentVariable("RQDQ__VIEWNG__DATA_DIR") ?? "data";
+    _sceneFileName = System.Environment.GetEnvironmentVariable("RQDQ__VIEWNG__SCENE_FILE_NAME") ?? "scene.json";
+    scene.AnyCompiler.Setup();
+    scene.GlMeshCompiler.DataDir = _dataDir;  // XXX yuck
+
+    _builtins.Add(_systemValues);
+    InitializeComponent(); }
+
+  private void MainForm_Load(object sender, EventArgs e) {
+    _desc = new() {
+      BufferCount = 1,
+      Flags = SwapChainFlags.None,
+      IsWindowed = true,
+      ModeDescription = new ModeDescription(
+        this.renderControl1.ClientSize.Width,
+        this.renderControl1.ClientSize.Height,
+        new Rational(60, 1),
+        Format.R8G8B8A8_UNorm),
+      OutputHandle = this.renderControl1.Handle,
+      SampleDescription = new SampleDescription(1, 0),
+      SwapEffect = SwapEffect.Discard,
+      Usage = Usage.RenderTargetOutput };
+
+    SharpDX.Direct3D11.Device.CreateWithSwapChain(
+      SharpDX.Direct3D.DriverType.Hardware,
+      DeviceCreationFlags.Debug,
+      _desc,
+      out _gpu, out _sc);
+
+    _renderTarget = Texture2D.FromSwapChain<Texture2D>(_sc, 0);
+    _renderTargetView = new RenderTargetView(_gpu, _renderTarget);
+    _depthBuffer = new Texture2D(_gpu, new Texture2DDescription() {
+      Format = Format.D32_Float_S8X24_UInt,
+      ArraySize = 1,
+      MipLevels = 1,
+      Width = renderControl1.ClientSize.Width,
+      Height = renderControl1.ClientSize.Height,
+      SampleDescription = new SampleDescription(1, 0),
+      Usage = ResourceUsage.Default,
+      BindFlags = BindFlags.DepthStencil,
+      CpuAccessFlags = CpuAccessFlags.None,
+      OptionFlags = ResourceOptionFlags.None, });
+    _depthBufferView = new DepthStencilView(_gpu, _depthBuffer);
+    _gpu.ImmediateContext.OutputMerger.SetTargets(_depthBufferView, _renderTargetView);
+
+    _mtime = File.GetLastWriteTime(Path.Join(_dataDir, _sceneFileName));
+    string sceneText = File.ReadAllText(Path.Join(_dataDir, _sceneFileName));
+    _runningGraph = scene.GraphBuilder.Build(sceneText, _builtins);
+    if (_runningGraph.root is null) {
+      throw new Exception("did not find a layer node with id __main__"); }
+    _runningGraph.Init(_gpu); }
+
+  private
+  void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+    _gpu?.Dispose();
+    _sc?.Dispose();
+    _renderTargetView?.Dispose();
+    _renderTarget?.Dispose(); }
+
+  private
+  void MainForm_Paint(object sender, PaintEventArgs e) { }
+
+  private
+  void renderControl1_Paint(object sender, PaintEventArgs e) {
+    if (_renderResized) {
+      _renderResized = false;
+
+      _renderTarget?.Dispose();
+      _renderTargetView?.Dispose();
+      _depthBufferView?.Dispose();
+      _depthBuffer?.Dispose();
+
+      _sc.ResizeBuffers(_desc.BufferCount,
+                        renderControl1.ClientSize.Width,
+                        renderControl1.ClientSize.Height,
+                        Format.Unknown,
+                        SwapChainFlags.None);
+
+      _renderTarget = Texture2D.FromSwapChain<Texture2D>(_sc, 0);
+      _renderTargetView = new RenderTargetView(_gpu, _renderTarget);
+      _depthBuffer = new Texture2D(_gpu, new Texture2DDescription() {
+        Format = Format.D32_Float_S8X24_UInt,
+        ArraySize = 1,
+        MipLevels = 1,
+        Width = renderControl1.ClientSize.Width,
+        Height = renderControl1.ClientSize.Height,
+        SampleDescription = new SampleDescription(1, 0),
+        Usage = ResourceUsage.Default,
+        BindFlags = BindFlags.DepthStencil,
+        CpuAccessFlags = CpuAccessFlags.None,
+        OptionFlags = ResourceOptionFlags.None, });
+      _depthBufferView = new DepthStencilView(_gpu, _depthBuffer); }
+
+    var dc = _gpu.ImmediateContext;
+    dc.Rasterizer.SetViewport(0, 0, this.renderControl1.ClientSize.Width, this.renderControl1.ClientSize.Height, 0.0F, 1.0F);
+    dc.OutputMerger.SetTargets(_depthBufferView, _renderTargetView);
+
+    _systemValues.Upsert("T", _clock.ElapsedMilliseconds / 1000.0F);
+    _systemValues.Upsert("canvasSize", new Vector2(renderControl1.ClientSize.Width, renderControl1.ClientSize.Height));
+    _systemValues.Upsert("pixelAspect", new Vector2(1, 1));
+
+    if (_depthBufferView is null) {
+        Console.WriteLine("dbv is null!");
+        throw new Exception("badness!"); }
+    dc.ClearDepthStencilView(_depthBufferView, DepthStencilClearFlags.Depth, 1.0F, 0);
+
+    if (_runningGraph.root is scene.ILayer rootLayer) {
+      var tmp = rootLayer.GetColor();
+      dc.ClearRenderTargetView(_renderTargetView, new RawColor4(tmp.X, tmp.Y, tmp.Z, 1.0F));
+      rootLayer.Draw(dc); }
+
+    _sc.Present(0, PresentFlags.None); }
+
+  private
+  void timer1_Tick(object sender, EventArgs e) {
+    this.renderControl1.Invalidate(); }
+
+  private
+  void renderControl1_Resize(object sender, EventArgs e) {
+    _renderResized = true; }
+
+  private
+  void quitToolStripMenuItem_Click(object sender, EventArgs e) {
+    this.Close(); } }
+
+
+}  // close package namespace
